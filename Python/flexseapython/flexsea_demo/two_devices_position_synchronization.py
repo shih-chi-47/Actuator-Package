939e20b8e8cb5167c251a018193961fadb353406
ShihChi <shihchil@umich.edu>
2020-04-08 22:59:11 -0400

    initial trial, position setpoint from each other

---
+++ Python/flexseapython/flexsea_demo/two_devices_position_synchronization.py
@@ -0,0 +1,79 @@
+import os, sys
+from time import sleep
+
+import traceback
+
+pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(pardir)
+from fxUtil import *
+
+def printDevice(actPackState):
+    print('State time: ', actPackState.timestamp)
+    print('Accel X: ', actPackState.accelx, ', Accel Y: ', actPackState.accely, ' Accel Z: ', actPackState.accelz)
+    print('Gyro X: ', actPackState.gyrox, ', Gyro Y: ', actPackState.gyroy, ' Gyro Z: ', actPackState.gyroz)
+    print('Motor angle: ', actPackState.encoderAngle, ', Motor voltage: ', actPackState.motorVoltage, flush=True)
+
+
+def fxLeaderFollower(leaderPort, followerPort, baudRate):
+
+    devId0 = fxOpen(leaderPort, baudRate, 0)
+    devId1 = fxOpen(followerPort, baudRate, 0)
+
+    fxStartStreaming(devId0, 200, True)
+    fxStartStreaming(devId1, 200, True)
+
+    sleep(0.2)
+
+    actPackState0 = fxReadDevice(devId0)	
+    actPackState1 = fxReadDevice(devId1)
+
+    initialAngle0 = actPackState0.encoderAngle
+    initialAngle1 = actPackState1.encoderAngle
+
+    kp = 100
+    kd = 20
+
+    fxSetGains(devId0, kp, kd, 0, 0, 0)
+    fxSendMotorCommand(devId0, FxPosition, initialAngle0)
+
+    fxSetGains(devId1, kp, kd, 0, 0, 0)
+    fxSendMotorCommand(devId1, FxPosition, initialAngle1)
+
+    count = 0
+    try:
+        while(True):
+            sleep(0.05)
+
+            Data0 = fxReadDevice(devId0)
+            Data1 = fxReadDevice(devId0)
+
+            angle0 = Data0.encoderAngle
+            angle1 = Data1.encoderAngle
+            
+            diff0 = angle0 - initialAngle0
+            fxSendMotorCommand(devId1, FxPosition, initialAngle1 + diff0)
+
+            diff1 = angle1 = initialAngle1
+            fxSendMotorCommand(devId0, FxPosition, initialAngle0 + diff1)
+
+            
+            print("device {} following device {}".format(devId1, devId0))
+            
+            printDevice(followerData)
+            printDevice(leaderData)
+
+    except Exception as e:
+        print(traceback.format_exc())
+
+    print('Turning off position control...')
+    fxClose(devId0)
+    fxClose(devId1)
+
+if __name__ == '__main__':
+    baudRate = sys.argv[1]
+    ports = sys.argv[2:4]
+    try:
+        fxLeaderFollower(ports[0], ports[1], baudRate)
+    except Exception as e:
+        print("broke: " + str(e))
+        pass
